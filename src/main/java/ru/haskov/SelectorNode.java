fd9f44e4219ee6433bfbcd5d0fdb89836fe0bcee
Хасков Егор Алексеевич <e.khaskov@g.nsu.ru>
2026-01-21 12:09:32 +0700

    Add split and selector node.

---
+++ src/main/java/ru/haskov/SelectorNode.java
@@ -0,0 +1,14 @@
+package ru.haskov;
+
+import java.util.Comparator;
+import java.util.List;
+
+public final class SelectorNode implements Node<List<Document>, Document> {
+
+    @Override
+    public Document process(List<Document> inputs) {
+        return inputs.stream()
+                .min(Comparator.comparingInt(d -> d.history().size()))
+                .orElseThrow();
+    }
+}
