95f202748db9c1686d1f35bb65690851dc76f8df
Roma <roma2002.rs@yandex.ru>
2026-01-24 12:01:42 +0700

    refactor names

---
+++ src/tests/java/PipelineLoaderTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import ru.haskov.NodeServer;
+import ru.haskov.PipelineLoader;
+import ru.haskov.nodes.MergeNode;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PipelineLoaderTest {
+    @Test
+    void loadPipelineFromJson() throws Exception {
+        String json = """
+        {
+          "dataType": "Document",
+          "nodes": [
+            {
+              "id": "normalize",
+              "type": "NormalizeNode",
+              "listenPort": 5001,
+              "targets": [5002]
+            },
+            {
+              "id": "merge",
+              "type": "MergeNode",
+              "listenPort": 5002,
+              "targets": [5003],
+              "expectedParts": 2
+            }
+          ]
+        }
+        """;
+
+        List<NodeServer<?, ?>> servers = PipelineLoader.load(json);
+
+        assertEquals(2, servers.size());
+        assertTrue(servers.get(1).getNode() instanceof MergeNode);
+    }
+}
