456f471e32baf5d01dc5d197f2fb91e848e4d556
Roma <roma2002.rs@yandex.ru>
2026-01-24 11:47:30 +0700

    create test directory with tests

---
+++ src/test/java/NormalizeNodeTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import ru.haskov.Document;
+import ru.haskov.nodes.NormalizeNode;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NormalizeNodeTest {
+    @Test
+    void normalizeOneDocument() {
+        NormalizeNode norm = new NormalizeNode();
+
+        Document d1 = new Document("txt", "Hello WORLD", List.of());
+
+        Document result = norm.process(d1);
+
+        assertNotNull(result);
+        assertEquals("hello world", result.content());
+        assertTrue(result.history().contains("normalize"));
+    }
+}
